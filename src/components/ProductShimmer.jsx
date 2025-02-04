c25ae36074eb399e5f6a2263c159c3f549f2d039
Ganesh1408 <somariganesh@gmail.com>
2025-02-04 14:11:48 +0530

    final

--- src/components/ProductShimmer.jsx
+++ src/components/ProductShimmer.jsx
@@ -3,13 +3,14 @@ import {  ProductContainer } from './ProductCard/styledComponent'
 import { SectionContainer } from './products/styledComponent'
 
 export default function ProductShimmer() {
-    const mapped=Array.from({length:20}).map((el,i)=>{
-        return <>
+    const mapped=Array.from({length:20},(_,i)=>(
+        
         <ProductContainer key={i} style={{border:"1px solid lightgray",backgroundColor:"lightgrey"}}>
             
         </ProductContainer>
-        </>
-    })
+        
+        
+    ))
   return (
     
     
